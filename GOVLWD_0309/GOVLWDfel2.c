c4c28579942cb29a9e0ecc5e81c0729ac5615a45
BMateLaszlo <99289974+BMateLaszlo@users.noreply.github.com>
2022-03-09 09:32:52 +0100

    Add files via upload

---
+++ GOVLWD_0309/GOVLWDfel2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	char kod[20];
+	printf("Kerek egy vegrehajtando kodot: ");
+	scanf("%s", kod);
+
+	system(kod);
+
+	return 0;
+
+}
---
+++ GOVLWD_0309/child.c
@@ -0,0 +1,11 @@
+#inlcude <stdio.h>
+#inlcude <stdlib.h>
+
+int main()
+{
+
+for (i=0;i<=10;i++){
+	printf("Bodnár Máté László, GOVLWD\n");
+}
+ return 2;
+}
